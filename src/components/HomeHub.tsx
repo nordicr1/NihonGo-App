6f9ee15c26f1e7ac1dc94073ecfad3e9337aac48
Ramon souza <ramonkyo@gmail.com>
2026-08-23 00:44:43 -0400

    chore: update changelog for N5 verbs

--- src/components/HomeHub.tsx
+++ src/components/HomeHub.tsx
@@ -58,8 +58,8 @@ const CHANGELOG = [
   },
   {
     date: '22 Ago 2026',
-    title: 'Mais de 1.000 Substantivos Adicionados!',
-    desc: 'Adicionamos 422 substantivos para o N5, 363 para o N4, 160 para o N3 e dezenas para o N2, todos com traduções, frases de exemplo exclusivas e áudio.',
+    title: 'Mais de 1.100 Palavras Adicionadas!',
+    desc: 'Adicionamos 100 verbos novos no N5 categorizados por tipo (Godan, Ichidan e Irregulares), além de mais de 1.000 substantivos divididos entre N5, N4, N3 e N2, todos com traduções, frases de exemplo exclusivas e áudio.',
   },
   {
     date: '22 Ago 2026',
